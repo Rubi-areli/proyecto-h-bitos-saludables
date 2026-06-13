c0445c8dfe240ee281337634ac2523738a0bb524
Rubi-areli <rubita.alvarenga@gmail.com>
2026-06-12 22:07:52 -0600

    Fix rutas app.py sin emojis

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ import streamlit as st
 pg = st.navigation(
     {
         "Portafolio": [
-            st.Page("pages/1_ 🏠page.py", title="Inicio"),
+            st.Page("pages/1_page.py", title="Inicio"),
         ],
         "Tarea 1": [
-            st.Page("pages/2_📊_EDA.py", title="EDA"),
-            st.Page("pages/4_🤖_MachineLearning.py", title="Machine Learning"),
-            st.Page("pages/7_🍳_Recomendacion.py", title="Recomendación"),
+            st.Page("pages/2_EDA.py", title="EDA"),
+            st.Page("pages/4_MachineLearning.py", title="Machine Learning"),
+            st.Page("pages/7_Recomendacion.py", title="Recomendación"),
         ],
         "Tarea 2": [
-            st.Page("pages/5_📂_Carga_Archivos.py", title="Carga de Archivos"),
-            st.Page("pages/6_😊_Sentimientos.py", title="Sentimientos"),
+            st.Page("pages/5_Carga_Archivos.py", title="Carga de Archivos"),
+            st.Page("pages/6_Sentimientos.py", title="Sentimientos"),
         ],
         "Opcional": [
-            st.Page("pages/8_🤖_IA.py", title="Asistente IA"),
+            st.Page("pages/8_IA.py", title="Asistente IA"),
         ],
     }
 )
